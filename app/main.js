cefe5989e482af52a3b10939c8f6f60869aa0dbe
Christian Dreier <flipper.x3r@googlemail.com>
2022-08-16 12:05:34 +0200

    Add logger
    Without a logger, i.e. just with console.log, it is not possible
    to get all messages from the main process.

--- app/main.js
+++ app/main.js
@@ -1,5 +1,9 @@
-const electron = require("electron")
 const path = require("path")
+
+const electron = require("electron")
+const log4js = require("log4js")
+
+let logger
 
 function createWindow() {
     const mainWindow = new electron.BrowserWindow({
@@ -9,10 +13,19 @@ function createWindow() {
             preload: path.join(__dirname, "preload.js"),
         },
     })
+    mainWindow.on("closed", () => logger.debug("Window closed"))
     mainWindow.loadFile(path.join(__dirname, "index.html"))
 }
 
 electron.app.whenReady().then(() => {
+    log4js.configure({
+        appenders: {
+            default: { type: "file", filename: path.join(__dirname, "..", "default.log") },
+        },
+        categories: { default: { appenders: ["default"], level: "debug" } },
+    })
+    logger = log4js.getLogger("default")
+
     createWindow()
 
     electron.app.on("activate", () => {
@@ -20,9 +33,12 @@ electron.app.whenReady().then(() => {
             createWindow()
         }
     })
+
+    logger.info("Application started")
 })
 
 electron.app.on("window-all-closed", () => {
+    logger.debug("Last window closed")
     if (process.platform !== "darwin") {
         electron.app.quit()
     }
